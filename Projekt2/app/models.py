d9e84d87008cc7c7c9c9441fdd044af2d82bfa25
EgurKrad <161019894+EgurKrad@users.noreply.github.com>
2026-04-23 12:37:51 +0200

    Usunięto niepotrzebne elementy

--- Projekt2/app/models.py
+++ Projekt2/app/models.py
@@ -1,41 +1,6 @@
 from app import db
 from enum import Enum
 
-class WeightCategory(Enum):
-    BELOW_1G = "<1g"
-    FROM_1_TO_15G = "1g-15g"
-    FROM_15_TO_50G = "15g-50g"
-    FROM_50G_TO_1KG = "50g-1kg"
-    FROM_1_TO_10KG = "1kg-10kg"
-    FROM_10_TO_30KG = "10kg-30kg"
-    FROM_30_TO_100KG = "30kg-100kg"
-    FROM_100_TO_500KG = "100kg-500kg"
-    ABOVE_500KG = ">500kg"
-
-class SizeCategory(Enum):
-    BELOW_5CM = "<5cm"
-    FROM_5_TO_20CM = "5cm-20cm"
-    FROM_20_TO_50CM = "20cm-50cm"
-    FROM_50_TO_100CM = "50cm-100cm"
-    FROM_100_TO_300CM = "100cm-300cm"
-    ABOVE_300CM = ">300cm"
-
-class Habitat(Enum):
-    FOREST = "las"
-    OCEAN = "ocean"
-    MOUNTAINS = "góry"
-    DESERT = "pustynia"
-    SAVANNA = "sawanna"
-
-class AnimalCategory(Enum):
-    FISH = "ryba"
-    ARACHNID = "pajęczak"
-    INSECT = "owad"
-    REPTILE = "gad"
-    AMPHIBIAN = "płaz"
-    MAMMAL = "ssak"
-    BIRD = "ptak"
-
 class Animal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
